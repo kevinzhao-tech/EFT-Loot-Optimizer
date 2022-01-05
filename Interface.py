a12791d334c8900075e084ca05dd007c172f6d64
C1intMason <zhao1939@gmail.com>
2022-01-05 11:51:01 -0800

    separated start_polling into separate methods

--- Interface.py
+++ Interface.py
@@ -1,21 +1,36 @@
 import keyboard
 import mouse
 import ScreenScraper
+import pytarkovdata
 
 
-def start_polling():
+def start_polling_optimal_trader():
     """
     Starts the polling of the keyboard and mouse for ocr near the mouse
     Prints, to the console, the ocr text when alt+click is pressed
     """
     while True:
-        mouse.wait('left')
-        if keyboard.is_pressed('alt'):
-            screenshot = ScreenScraper.get_screenshot_near_mouse()
+        print(poll_mouse_for_optimal_trader())
 
-            result = ScreenScraper.parse_click(screenshot)
-            print(result)
+
+def poll_mouse_for_optimal_trader():
+    """
+    polls, once, the keyboard and mouse for ocr near the mouse
+    Prints, to the console, the ocr text when alt+click is pressed
+    """
+
+    mouse.wait('left')
+    if keyboard.is_pressed('t'):
+        screenshot = ScreenScraper.get_screenshot_near_mouse()
+
+        result = ScreenScraper.parse_click(screenshot)
+        result = result.replace('\n', '')
+        print(result)
+        try:
+            return pytarkovdata.get_optimal_trader_by_name(result)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
-    start_polling()
+    start_polling_optimal_trader()
